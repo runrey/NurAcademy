4d4a884228d6651bae7634acf497c67b740da465
ChpOlzhik <57743402+ChpOlzhik@users.noreply.github.com>
2021-04-29 15:55:29 +0600

    Created Constructor main page, creating new course, updating course. Added to views try catch block

--- Constructor/urls.py
+++ Constructor/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'constructor'
+
 urlpatterns = [
+    path('', views.mainPage, name='mainPage'),
     path('new-course', views.newCourse, name='newcourse'),
-    path('update-course/<str:id>', views.updateMyCourse, name='updatecourse'),
+    path('update-course/<str:course_id>', views.updateMyCourse, name='updatecourse'),
 
 ]
--- Constructor/views.py
+++ Constructor/views.py
@@ -1,10 +1,41 @@
 from django.http import HttpResponse
-import datetime
+from django.shortcuts import render, redirect
+from .forms import NewCourseForm
+from .models import Course,UserCourse, User
+def mainPage(request):
+    try:
+        mail = request.session['email']
+        if mail is None:
+            raise Exception
+        courses = Course.objects.filter(usercourse__user__Email=mail)
+        context = {
+            'courses': courses,
+        }
+        return render(request=request, template_name='constructor/mainPage.html', context=context)
+    except Exception as e:
+        print(e)
+        return redirect('login')
 
 
 def newCourse(request):
-    pass
+    form = NewCourseForm()
+    context = {
+        'form': form,
+    }
+    return render(request=request, template_name='constructor/newcourse.html', context=context)
 
 
-def updateMyCourse(request, id):
-    pass
+def updateMyCourse(request, course_id):
+    try:
+        course = Course.objects.get(id=course_id, usercourse__user__Email=request.session['email'], usercourse__Action=True)
+    except Exception as e:
+        return HttpResponse("It is none of your bussiness")
+
+    data = course.__dict__
+    form = NewCourseForm(initial=data)
+    context = {
+        'form': form,
+        'course_id': course.id
+    }
+    return render(request=request,template_name='constructor/updatecourse.html', context=context)
+
