c04e50282c0ab9c827bd98d29012c8e6be07eaab
runrey <ernur.bolatkanov@yandex.ru>
2021-04-29 08:55:57 +0600

    Profile page

--- Courses/urls.py
+++ Courses/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('sign', views.sign, name='sign'),
-    path('profile/', views.profile, name='profile'),    
+    path('profile/', views.profile, name='profile'),
 ]
