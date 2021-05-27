9f3027e0c08362762b419a0e7cdd7b2d77e697be
Nurgissa <nurike2001@mail.ru>
2021-05-27 23:35:25 +0600

    moderating data taking

--- Moderating/views.py
+++ Moderating/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 import requests
+from Courses.models import User, UserCourse, Course
 
 
 def moderating(request):
-    url = 'http://127.0.0.1:8000/api/inactive-courses/'
-    courses = requests.get(url=url).json()
+    courses = Course.objects.filter(Status='IA')
+    coursesNEW = {}
+    for course in courses:
+        usrCourse = UserCourse.objects.filter(course=course).first()
+        if usrCourse.Action:
+            coursesNEW[course] = usrCourse.user.Username
     con = {
-        'courses': courses,
+        'courses': coursesNEW,
     }
-    return render(request, template_name='moderator/moderating.html', context=con)
 
+    return render(request, template_name='moderator/moderating.html', context=con)
